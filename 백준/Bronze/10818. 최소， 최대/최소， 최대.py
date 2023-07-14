a6c530bacc8a03a18b4e8a8adb612746a24149eb
hoyoul (study github) <hoyoul.park@gmail.com>
2023-07-14 22:02:09 +0900

    Update 최소， 최대.py

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,3 +1,14 @@
-N  = int(input())
-L = list(map(int,input().split(' ')))
-print(min(L),max(L))
+# 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 
+# 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 
+#모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+N = int(input())
+L = list(map(int, input().split(' ')))
+min = 1000000
+max = -1000000
+for i in range(N):
+    if L[i] > max:
+        max = L[i]
+    if L[i] < min:
+        min = L[i]
+print(min,max)
